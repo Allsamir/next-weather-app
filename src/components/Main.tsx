d3ed7bc0b4d7e8b92425e277a5ec32b5dec46e5c
Al Samir <shahinur1591@gmail.com>
2024-06-18 15:58:28 +0600

    moment.js added

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Navbar from "./Navbar";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
+import moment from "moment";
 interface WeatherResponse {
   cod: string;
   message: number;
@@ -88,10 +89,41 @@ function Main({}: Props) {
         )
       ).data,
   });
-  console.log(data?.city.country);
+
+  const firstData = data?.list[0];
+
+  if (isPending) {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-black"></div>
+      </div>
+    );
+  }
+  if (isError) {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <h1 className="text-2xl font-bold">
+          Something Went Wrong Reload The Page
+        </h1>
+      </div>
+    );
+  }
   return (
     <div className="flex flex-col bg-gray-100">
       <Navbar />
+      <main className="px-3 w-full max-w-7xl mx-auto flex flex-col gap-10 pb-10 pt-4">
+        <section className="today_data">
+          <div>
+            <h2 className="flex gap-1 text-2xl items-start">
+              <p>{moment(firstData?.dt_txt).format("dddd")}</p>
+              <p>
+                ({moment(firstData?.dt_txt).format("l").split("/").join(".")})
+              </p>
+            </h2>
+          </div>
+        </section>
+        <section className="sevenDays_data"></section>
+      </main>
     </div>
   );
 }
